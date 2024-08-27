8c37fe05f4523cb9a46a19271a9c4e383b670e4e
SamkeloPatience <141482785+SamkeloPatience@users.noreply.github.com>
2024-08-27 05:59:02 +0200

    27/08/224

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -22,70 +22,46 @@ export default function Login() {
 
   return (
     <main>
-        <div className={``}>
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className={`col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center bg-light ${styles.container}`}>
-              <div className={styles.card}>
-                <div className="row g-0">
-                  <div className="col-md-8 col-lg-9 d-flex align-items-center">
-                    <div className="card-body p-6 p-lg-8 text-black">
-                      <form onSubmit={handleSubmit} autoComplete="off">
-                        <div className="d-flex align-items-center mb-2 pb-1">
-                          <i className="fas fa-cubes fa-2x me-3"></i>
-                          <div className={`h1 fw-bold mb-0 `}>
-                            <Image
-                              className={` justify-content-end${styles.LogoImage}`}
-                              src={Logo}
-                              width={80}
-                              height={50} 
-                              alt="Logo" 
-                            />
-                          </div>
-                        </div>
-                        <div className="form-outline mb-2">
-                          <label className="form-label" htmlFor="form2Example17">
-                            Username
-                          </label>
-                          <input
-                            type="username"
-                            id="form2Example17"
-                            className="form-control form-control-lg"
-                            value={Username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            autoComplete="off" 
-                            required
-                          />
-                        </div>
-                        <div className="form-outline mb-1">
-                        <label className="form-label" htmlFor="form2Example27">Password</label>
-                          <input
-                            type="password"
-                            id="form2Example27"
-                            className="form-control form-control-lg"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            autoComplete="new-password" // Prevents browser from autofilling or saving this input
-                            required
-                          />
-                        </div>
+      <section className={`vh-100 ${styles.section}`}>
+  <div className={`container py-5 h-100`}>
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col col-xl-10">
+        <div className={`${styles.card}`}>
+          <div className="row g-0">
+            <div className="col-md-6 col-lg-5 d-none d-md-block">
+              <Image src={Logo}
+                alt="login form" className={`img-fluid `}/>
+            </div>
+            <div className={`col-md-6 col-lg-7 d-flex align-items-center ${styles.Login}`}>
+              <div className="card-body p-4 p-lg-5 text-black">
 
-                        <div className="pt-1 mb-4">
-                          <button className="btn btn-dark btn-lg btn-block" type="submit">
-                            Login
-                          </button>
-                        </div>
-
-                        <a className="small text-muted" href="#!">
-                          Forgot password?
-                        </a>
-                      </form>
-                    </div>
+                <form>
+                  <div data-mdb-input-init className="form-outline mb-4">
+                  <label className="form-label" for="form2Example17">Username</label>
+                    <input type="email" id="form2Example17" className="form-control form-control-lg" />
                   </div>
-                </div>
+
+                  <div data-mdb-input-init className="form-outline mb-4">
+                  <label className="form-label" for="form2Example27">Password</label>
+                    <input type="password" id="form2Example27" className="form-control form-control-lg" />
+                  </div>
+
+                  <div className="pt-1 mb-4">
+                    <button data-mdb-button-init data-mdb-ripple-init className="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                  </div>
+                  <a className="small text-muted" href="#!">Forgot password?</a>
+                 
+                </form>
+
               </div>
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  </div>
+</section>
+
     </main>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Navbar from "@components/Navbar"
+import Navbar from "@components/Navbar";
 
 export default function Home() {
   return (
     <main className={styles.home}>
-          <Navbar/>
-         <h1>This is a home page </h1>
-         <h1>Msizi</h1>
+      <Navbar />
+      
+
     </main>
   );
 }
