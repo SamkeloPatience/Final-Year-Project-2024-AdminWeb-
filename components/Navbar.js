5ea65babdf27552a6d13f76cac155db7646034e4
SamkeloPatience <ncubesamkelo82@gmail.com>
2024-09-19 09:43:23 +0200

    19/09/21

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,18 +10,18 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <main>
-      <nav className={` navbar fixed-top navbar-expand-sm text-bg-info ${styles.nav} `}>
+      <nav className={` navbar navbar-expand-sm text-bg-info ${styles.nav} `}>
         <ul>
           <Image
             src={logo}
-            width={60}
-            height={50}
+            width={30}
+            height={20}
             alt="unizulu"
             className={`navbar-brand ${styles.navImg}`}
           />
           <h6 className={`text-left text-dark`}>admin@unizulu</h6>
         </ul>
-        <div className={`col-sm-12 col-md-8 col-lg-6 justify-content-end ${styles.navlink}`}>
+        <div className={`col-sm-12 col-md-8 col-xlg-6 justify-content-end ${styles.navlink}`}>
           <ul className={`navbar-nav  `}>
             <li className="nav-item">
               <a className="nav-link" href="dashboard">
